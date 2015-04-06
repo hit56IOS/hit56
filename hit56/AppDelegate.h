81642b44273ae18cace08bce85d83232e8859875
deyingong <gongdeyin@blinnnk.com>
2015-04-06 16:46:42 +0800

    Initial Commit

---
+++ hit56/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hit56
+//
+//  Created by gongdeyin on 4/6/15.
+//  Copyright (c) 2015 anlu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hit56/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hit56
+//
+//  Created by gongdeyin on 4/6/15.
+//  Copyright (c) 2015 anlu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
